0ae940497959d6e44e49407166eb2f18d584f17e
romzyyy <fijay0117@gmail.com>
2023-01-10 21:34:45 +0700

    add section for layout blade templet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
-          <h1>ini adalah halaman home</h1>
-          <h2>Selamat datang, {{ $name }}. anda adalah {{ $role }}</h2>
+@extends('layouts.mainlayout')
+
+@section('name', 'home')
+
+@section('content')
+<h1>ini adalah halaman home</h1>
+<h2>Selamat datang, {{ $name }}. anda adalah {{ $role }}</h2>
 
 {{-- @if($role == 'admin')
 <a href="">kehalaman admin</a>
@@ -8,34 +13,37 @@
 @else
 <a href="">kehalaman terserah</a>
 @endif --}}
-            {{-- @switch($role)
-                @case($role == 'admin')
-                    <a href="">kehalaman admin</a>
-                    @break
-                @case($role == 'staff')
-                    <a href="">kehalaman staff</a>
-                    @break
-                @default
-                    <a href="">kehalaman apasaja</a>
-            @endswitch --}}
+  {{-- @switch($role)
+      @case($role == 'admin')
+          <a href="">kehalaman admin</a>
+          @break
+      @case($role == 'staff')
+          <a href="">kehalaman staff</a>
+          @break
+      @default
+          <a href="">kehalaman apasaja</a>
+  @endswitch --}}
 
-            {{-- @for($i = 0; $i < 5; $i++)
-            {{ $i }} <br>
-            @endfor --}}
+  {{-- @for($i = 0; $i < 5; $i++)
+  {{ $i }} <br>
+  @endfor --}}
 
-            {{-- @foreach ($buah as $data)
-                {{ $data }} <br>
-            @endforeach --}}
+  {{-- @foreach ($buah as $data)
+      {{ $data }} <br>
+  @endforeach --}}
 
-            <table class="table">
-                <tr>
-                    <th>No.</th>
-                    <th>Nama</th>
-                </tr>
-                @foreach($buah as $item)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $item }}</td>
-                </tr>
-                @endforeach
-            </table>
+  <table class="table">
+      <tr>
+          <th>No.</th>
+          <th>Nama</th>
+      </tr>
+      @foreach($buah as $item)
+      <tr>
+          <td>{{ $loop->iteration }}</td>
+          <td>{{ $item }}</td>
+      </tr>
+      @endforeach
+  </table>
+@endsection
+
+         
